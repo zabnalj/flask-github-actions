b1e55c5a4f877d5614d11eb64b8ecde99d1942c8
faialotaibi <f.alotaibi@tuwaiq.edu.sa>
2024-10-07 19:24:17 +0300

    project added

---
+++ test_app.py
@@ -0,0 +1,13 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_hello_world(client):
+    rv = client.get('/')
+    assert rv.data == b'Hello, World!'
+
